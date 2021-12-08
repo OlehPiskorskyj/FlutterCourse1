4aeea7a96b6d75927674c9a8c1018bfb75fd7946
OlehPiskorskyj <oleh.piskorskyj@gmail.com>
2021-12-08 15:22:59 +0100

    - initial commit & push. Position in course: lesson 44.

---
+++ flutter_complete_guide/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import './qestion.dart';
+import './answer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What is your favorite color?",
+      "What is your favorite animal?"
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex],
+            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+          ],
+        ),
+      ),
+    );
+  }
+}
